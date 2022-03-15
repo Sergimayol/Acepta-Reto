73d7979a7a815e5f00afa1f20b7366e7b9b58b99
Sergi Mayol <sergi2808@gmail.com>
2022-03-15 23:45:00 +0100

    updated

---
+++ p100to199/P116.java
@@ -0,0 +1,20 @@
+package javaapplication34;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergi
+ */
+public class P116 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n > 0) {
+            System.out.println("Hola mundo.");
+            n--;
+        }
+    }
+
+}
