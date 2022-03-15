bdd1b3858d240a472c01f83b4be1dba41a310077
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2022-03-15 23:49:25 +0100

    Update P116.java

--- p100to199/P116.java
+++ p100to199/P116.java
@@ -11,9 +11,8 @@ public class P116 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        while (n > 0) {
+        for (int i = 0; i < n; i++) {
             System.out.println("Hola mundo.");
-            n--;
         }
     }
 
