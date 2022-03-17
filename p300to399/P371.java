fdd144afd3fa2aa302059d163279dbc8d8184976
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2022-03-17 13:56:17 +0100

    Create P371.java

---
+++ p300to399/P371.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergi
+ */
+public class P371 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n;
+        while ((n = s.nextInt()) != 0) {
+            int c = 0;
+            for (int i = n; i > 0; i--) {
+                c += 3 * i;
+            }
+            System.out.println(c);
+        }
+    }
+
+}
