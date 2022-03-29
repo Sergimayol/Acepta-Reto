a77c5455630d08d2c273f6f6eec0f5b00ae14228
Sergi Mayol <sergi2808@gmail.com>
2022-03-29 13:24:52 +0200

    Create P544.cpp

---
+++ p500to599/P544.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	long long int p;
+	while (cin >> n >> p) {
+		long long int valor;
+		long long int maximo = 0;
+		int numUvas = 0;
+		for (int i = 0; i < n; ++i) {
+			cin >> valor;
+			if (valor + maximo <= p) {
+				numUvas++;
+				if (valor > maximo) maximo = valor;
+			}
+			else if (valor < maximo) maximo = valor;
+		}
+		cout << numUvas << '\n';
+	}
+
+	return 0;
+}
