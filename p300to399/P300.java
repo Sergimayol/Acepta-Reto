ab210520ba23deeff010fc67966780e09ed1b821
Sergi Mayol <83218617+Sergimayol@users.noreply.github.com>
2022-03-29 14:08:05 +0200

    Update P300.java

--- p300to399/P300.java
+++ p300to399/P300.java
@@ -1,4 +1,11 @@
-import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p300to399;
+
+import java.io.DataInputStream;
+import java.io.IOException;
 
 /**
  *
@@ -6,11 +13,83 @@ import java.util.Scanner;
  */
 public class P300 {
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    static class Reader {
+
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                    BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null) {
+                return;
+            }
+            din.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Reader s = new Reader();
         final int n = s.nextInt();
         for (int i = 0; i < n; i++) {
-            String p = s.next();
+            String p = s.readLine();
             if (p.contains("a") && p.contains("e")
                     && p.contains("i") && p.contains("o")
                     && p.contains("u")) {
