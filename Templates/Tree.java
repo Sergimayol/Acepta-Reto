3e472e2be3e2b6e208785c6ba212688836406c6d
Sergi Mayol <sergi2808@gmail.com>
2022-09-08 00:39:14 +0200

    Create Tree.java

---
+++ Templates/Tree.java
@@ -0,0 +1,43 @@
+package Templates;
+
+import java.util.ArrayList;
+
+public class Tree {
+
+    private static int h;
+    private static int k;
+
+    static class TreeNode {
+        ArrayList<TreeNode> tn;
+
+        public TreeNode() {
+            tn = new ArrayList<>();
+        }
+    }
+
+    static void addNode(TreeNode n, String[] in) {
+        if (k == in.length) {
+            return;
+        }
+        int hj = Integer.parseInt(in[k++]);
+        for (int i = 0; i < hj; i++) {
+            n.tn.add(new TreeNode());
+        }
+        for (TreeNode aux : n.tn) {
+            addNode(aux, in);
+        }
+    }
+
+    static void getHeight(TreeNode n, int t) {
+        if (n.tn.isEmpty()) {
+            if (t > h) {
+                h = t;
+                return;
+            }
+        }
+        for (TreeNode aux : n.tn) {
+            getHeight(aux, t + 1);
+        }
+    }
+
+}
