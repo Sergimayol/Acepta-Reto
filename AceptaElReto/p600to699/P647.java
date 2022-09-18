3dea9946550774afdf246f200c839f66ecf3623e
Sergi Mayol <sergi2808@gmail.com>
2022-09-18 13:23:33 +0200

    Create P647.java

---
+++ AceptaElReto/p600to699/P647.java
@@ -0,0 +1,176 @@
+package AceptaElReto.p600to699;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class P647 {
+
+    static Reader in;
+    static PrintWriter out;
+
+    static class Solver {
+
+        // Solution to problem goes here
+        Solver() throws IOException {
+            int c = in.nextInt();
+            while (c-- > 0) {
+                int nc = in.nextInt();
+                int ans = 0;
+                Set<Integer> map = new HashSet<>();
+                int[] arr = new int[nc];
+                for (int i = 0; i < nc; i++) {
+                    arr[i] = in.nextInt();
+                }
+                for (int i = 0, j = 0; i < nc; i++) {
+                    if (map.contains(arr[i])) {
+                        while (arr[j] != arr[i]) {
+                            map.remove(arr[j++]);
+                        }
+                        j++;
+                    } else {
+                        map.add(arr[i]);
+                        ans = Math.max(ans, i - j + 1);
+                    }
+                }
+                out.println(ans);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        in = new Reader();
+        out = new PrintWriter(System.out);
+        new Solver();
+        out.close();
+    }
+
+    static class Reader {
+
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(
+                    new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg) {
+                c = read();
+            }
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg) {
+                return -ret;
+            }
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                    BUFFER_SIZE);
+            if (bytesRead == -1) {
+                buffer[0] = -1;
+            }
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead) {
+                fillBuffer();
+            }
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null) {
+                return;
+            }
+            din.close();
+        }
+    }
+
+}
